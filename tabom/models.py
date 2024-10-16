51c7566d31a79d82b6c47884ad1b434de3204bab
jingangkim <seeu4534@gmail.com>
2024-10-16 11:26:59 +0900

    commit add BaseModel

--- tabom/models.py
+++ tabom/models.py
@@ -1,21 +1,23 @@
 from django.db import models
 
 
+class BaseModel(models.Model):
+    update_at = models.DateTimeField(auto_now=True)  # 마지막 수정 시간
+    created_at = models.DateTimeField(auto_now_add=True)  # 마지막 생성 시간
+
+    class Meta:
+        abstract = True
+
+
 # Create your models here.
-class User(models.Model):
+class User(BaseModel):
     name = models.CharField(max_length=50)  # 이름, 글자제한 50
-    update_at = models.DateTimeField(auto_now=True)  # 마지막 수정 시간
-    created_at = models.DateTimeField(auto_now_add=True)  # 마지막 생성 시간
 
 
-class Article(models.Model):
+class Article(BaseModel):
     title = models.CharField(max_length=255)  # 타이틀, 글자제한 50
-    update_at = models.DateTimeField(auto_now=True)  # 마지막 수정 시간
-    created_at = models.DateTimeField(auto_now_add=True)  # 마지막 생성 시간
 
 
-class Like(models.Model):
+class Like(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    update_at = models.DateTimeField(auto_now=True)  # 마지막 수정 시간
-    created_at = models.DateTimeField(auto_now_add=True)  # 마지막 생성 시간
