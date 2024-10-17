2f7e36cd63849a126d61210abab7ea28663f9828
jingangkim <seeu4534@gmail.com>
2024-10-17 14:10:49 +0900

    add undo_like

--- tabom/service/like_service.py
+++ tabom/service/like_service.py
@@ -4,5 +4,13 @@ from tabom.models import Like
 def do_like(user_id: int, article_id: int) -> Like:
     return Like.objects.create(user_id=user_id, article_id=article_id)
 
-
-# 우선 like가 있는지를 확인한다? if
+#좋아요 취소 기능, 자신의 좋아요를 취소하기 or 좋아요가 없다면 불가능하게
+def undo_like(user_id: int, article_id: int) -> None:
+    # try:
+    #     like = Like.objects.filter(user_id=user_id, article_id=article_id).get()  #우선 좋아요가 있는지를 가져오기 있다면? 삭제
+    #     like.delete()
+    # except Exception as e:
+    #     print(e)
+    #      쿼리를 두번 날렸다.
+    Like.objects.filter(user_id=user_id, article_id=article_id).delete()
+    #쿼리를 한 번만 날릴 수 있다.
--- tests/test_like_service.py
+++ tests/test_like_service.py
@@ -2,7 +2,7 @@ from django.db import IntegrityError
 from django.test import TestCase
 
 from tabom.models import Article, Like, User
-from tabom.service.like_service import do_like
+from tabom.service.like_service import do_like, undo_like
 
 
 class TestLikeService(TestCase):
@@ -29,3 +29,27 @@ class TestLikeService(TestCase):
         do_like(user.id, article.id)
         with self.assertRaises(IntegrityError):
             do_like(user.id, article.id)
+
+    def test_like_count_should_increase(self) -> None:
+
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test_title")
+
+        article_count = Article.objects.count()
+        #~ 할 때
+        do_like(user.id, article.id)
+
+        #~와 같은 가
+        articles = Article.objects.get(id=article.id)
+        self.assertEqual(1, articles.like_set.count())
+
+    def test_a_user_can_undo_like(self) -> None:
+
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test_title")
+        like = Like.objects.create(user_id=user.id, article_id=article.id)
+
+        undo_like(user.id, article.id)
+
+        with self.assertRaises(Like.DoesNotExist):
+            Like.objects.get(id=like.id)
